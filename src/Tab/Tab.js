aef6d0807ca5eaae527c540042fee7a147351c79
heonq <gkagjsrb@naver.com>
2023-11-22 14:38:13 +0900

    feat: 탭 선택 버튼을 누르면 탭을 표시하는 기능 구현

--- src/Tab/Tab.js
+++ src/Tab/Tab.js
@@ -1,12 +1,22 @@
 import $ from '../../utils/querySelector.js';
 import HEADER_MARKUP from './TabComponent.js';
+import { SELECT_COURSE_MARKUP } from '../CrewManaging/CrewManagingComponent.js';
+import { MANAGING_TEAM_MATCHING_MARKUP } from '../TeamMatching/TeamMatchingComponent.js';
 
 class Tab {
   initTab() {
     $('body').insertAdjacentHTML('beforebegin', HEADER_MARKUP);
+    $('#crew-tab').addEventListener('click', this.toggleManageCrewTab);
+    $('#team-tab').addEventListener('click', this.toggleManageTeamTab);
   }
 
-  toggleManageCrewTab() {}
+  toggleManageCrewTab() {
+    $('#app').innerHTML = SELECT_COURSE_MARKUP;
+  }
+
+  toggleManageTeamTab() {
+    $('#app').innerHTML = MANAGING_TEAM_MATCHING_MARKUP;
+  }
 }
 
 export default Tab;
